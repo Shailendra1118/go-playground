549f637201b0a74f2f00bc019348546143d19740
Shailendra <shailendra1118@outlook.com>
2023-07-14 17:32:46 +0530

    fibbonacci test

--- cmd/main.go
+++ cmd/main.go
@@ -6,4 +6,25 @@ import (
 
 func main() {
 	fmt.Println("Hello Shailendra Singh Yadav")
+
+	//closure testing
+	f := fibbonacci()
+
+	fmt.Println(f())
+	fmt.Println(f())
+	fmt.Println(f())
+	fmt.Println(f())
+	fmt.Println(f())
+}
+
+func fibbonacci() func() int {
+	last := 1
+	secondLast := 0
+
+	return func() int {
+		current := last + secondLast
+		secondLast = last
+		last = current
+		return last
+	}
 }
