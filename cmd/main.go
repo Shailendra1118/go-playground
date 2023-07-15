e4d9889c7bf42516cd11a697414c8d703d556485
Shailendra <shailendra1118@outlook.com>
2023-07-15 13:49:39 +0530

    basics

--- cmd/main.go
+++ cmd/main.go
@@ -4,6 +4,17 @@ import (
 	"fmt"
 )
 
+type Vertex struct {
+	X int
+	Y int
+}
+
+// Pointer receivers
+func (r *Vertex) DoubleIt() {
+	r.X *= 2
+	r.Y *= 2
+}
+
 func main() {
 	fmt.Println("Hello Shailendra Singh Yadav")
 
@@ -12,9 +23,28 @@ func main() {
 
 	fmt.Println(f())
 	fmt.Println(f())
-	fmt.Println(f())
-	fmt.Println(f())
-	fmt.Println(f())
+
+	//struct
+	v := Vertex{}
+	fmt.Println(v)
+
+	vPtr := &Vertex{}
+	vPtr.X = 100
+	vPtr.Y = 200
+	fmt.Println(*vPtr)
+
+	var v1 = &Vertex{}
+	v1.X = 44
+	v1.Y = 55
+	fmt.Println("V1 pointer", v1)
+	fmt.Println("V1 value", *v1)
+
+	var v2 = &Vertex{}
+	v2.X = 10
+	v2.Y = 20
+	v2.DoubleIt()
+	fmt.Println("After doubling it", *v2)
+
 }
 
 func fibbonacci() func() int {
